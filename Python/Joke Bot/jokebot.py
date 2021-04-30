481aaacc5f4859883bdee6da07b9614bfcb5b792
psurge1 <suraj.sswamy@gmail.com>
2021-04-30 13:15:40 -0500

    Update jokebot.py

--- Python/Joke Bot/jokebot.py
+++ Python/Joke Bot/jokebot.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix = '')
 
 # client.remove_command('help')
 
-status = cycle(['Pranking','Trolling'])
+status = cycle(['Hi','Hello','Sup','Howdy'])
 
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
